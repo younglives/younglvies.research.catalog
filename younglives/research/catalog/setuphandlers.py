6bfe90add979956268b9a36d4490573c831ac231
Michael Davis <m.r.davis@me.com>
2012-01-22 14:31:33 +0000

    Reindex the catalog on reinstall

--- younglives/research/catalog/setuphandlers.py
+++ younglives/research/catalog/setuphandlers.py
@@ -2,17 +2,18 @@ from StringIO import StringIO
 from Products.CMFCore.utils import getToolByName
 
 def configureArchetypeTool(portal, out):
-    """
-    Configure the archetype tool for the library enquiry catalog
-    """
+    """Configure the archetype tool for the library enquiry catalog"""
     at_tool = getToolByName(portal, 'archetype_tool')
     at_tool.setCatalogsByType('Author', ['portal_catalog', 'research_database_catalog'])
     at_tool.setCatalogsByType('Research', ['portal_catalog', 'research_database_catalog'])
 
+def reindexCatalog(portal, out):
+    """Reindex the research database catalog"""
+    research_database_catalog = getToolByName(portal, 'research_database_catalog')
+    research_database_catalog.clearFindAndRebuild()
+
 def setupVarious(context):
-    """
-    Import various settings.
-    """
+    """Import various settings."""
     # Only run step if a flag file is present
     if context.readDataFile('younglives.research.catalog_various.txt') is None:
         return
@@ -20,3 +21,4 @@ def setupVarious(context):
     out = StringIO()
 
     configureArchetypeTool(site, out)
+    reindexCatalog(site, out)
